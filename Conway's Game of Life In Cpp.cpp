100f061469886f308ebc57733944d2c58a9fd0d2
Saeed-Soukiah <saeedsoukiah@gmail.com>
2025-03-10 00:48:22 +0300

    Conway's Game of Life In Cpp.cpp

--- Conway's Game of Life In Cpp.cpp
+++ Conway's Game of Life In Cpp.cpp
@@ -1,9 +1,4 @@
 /* 
-* Game of Life Simulation * 
-* This program simulates Conway's Game of Life using a grid of cells. 
-* Users can interact with the simulation by toggling cells, starting, stopping, 
-* and adjusting the speed of the simulation. 
-* 
 * Author: Saeed Soukiah
 * Date: December 29, 2024 
 */
